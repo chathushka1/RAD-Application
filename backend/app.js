a21f56c79b897eba743d2f0e1e4628475de2646f
chathushka1 <www.chathushkamadumal123@gmail.com>
2024-06-25 12:07:44 +0530

    product done

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var signUserRouter = require('./routes/SignUser');
-
+var productRouter = require('./routes/Product');
 
 var app = express();
 
@@ -34,7 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/signUser', signUserRouter);
-
+app.use('/product', productRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
